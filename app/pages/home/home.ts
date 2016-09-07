581b3ef660233e81ab56d3ad409724caaf0f4133
Colin Palmer <Colin Palmer>
2016-09-07 22:40:30 +0100

    qr code example

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {QRCodeComponent} from 'ng2-qrcode'
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  directives: [QRCodeComponent],
 })
 export class HomePage {
   constructor(private navCtrl: NavController) {
-  
+
   }
 }
